9ea450e62bfefe1f31535258cf20bec324e0d88a
David Gomez <ldavid.gomez@gmail.com>
2020-05-06 14:25:13 +0200

    Add endpoint to get users

--- src/main/java/com/backend/firstbackend/resources/testController.java
+++ src/main/java/com/backend/firstbackend/resources/testController.java
@@ -1,5 +1,8 @@
 package com.backend.firstbackend.resources;
 
+import com.backend.firstbackend.domain.User;
+import com.backend.firstbackend.infrastructure.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/")
 public class testController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/hello-world")
     public ResponseEntity<String> helloWorld() {
         return ResponseEntity.ok("Hello world");
     }
 
+    @GetMapping("/users")
+    public ResponseEntity<Iterable<User>> getUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
 }
