f0d85a292fe40408c096fa5d00430d13f10fe805
David Gomez <ldavid.gomez@gmail.com>
2020-05-06 12:59:26 +0200

    Add user entity

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,17 @@
+package domain;
+
+import javax.persistence.*;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
