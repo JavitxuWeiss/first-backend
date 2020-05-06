4f561a144e49944a669b4f044e8ed48554e6bb9b
David Gomez <ldavid.gomez@gmail.com>
2020-05-06 14:24:39 +0200

    Enable jpa repositories

--- src/main/java/com/backend/firstbackend/FirstBackendApplication.java
+++ src/main/java/com/backend/firstbackend/FirstBackendApplication.java
@@ -2,7 +2,11 @@ package com.backend.firstbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.backend.firstbackend.infrastructure")
+@EntityScan("com.backend.firstbackend.domain")
 @SpringBootApplication
 public class FirstBackendApplication {
 
