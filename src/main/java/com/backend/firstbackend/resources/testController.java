aaeb0c39c9926bab861ae2ff17982ecea8e6be53
David Gomez <ldavid.gomez@gmail.com>
2020-05-06 12:43:36 +0200

    Add first controller

---
+++ src/main/java/com/backend/firstbackend/resources/testController.java
@@ -0,0 +1,17 @@
+package com.backend.firstbackend.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/")
+public class testController {
+
+    @GetMapping("/hello-world")
+    public ResponseEntity helloWorld() {
+        return ResponseEntity.ok("Hello world");
+    }
+
+}
