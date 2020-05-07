26aa55fd963ada58d30a828dc1d9bddf60dd66d6
David Gomez <ldavid.gomez@gmail.com>
2020-05-07 16:35:30 +0200

    Add user to migration

--- src/main/resources/db/scripts/create-user.sql
+++ src/main/resources/db/scripts/create-user.sql
@@ -1,7 +1,9 @@
 create table user
 (
-    id int not null,
+    id int not null auto_increment,
     name varchar(50) not null,
     email varchar(200) not null unique ,
         primary key (id)
 ) charset = utf8;
+
+INSERT INTO user (name, email) values ('david','ldavid.gomez@gmail.com');
